f90ab3e015a4145c4340a829fe61b64452560278
Ismael2301 <iasimbana1@espe.edu.ec>
2023-03-17 09:27:38 -0500

    Create module

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'VoluntariosCooperativos',
+    'summary': 'Direccion de una tienda cooperativa que venda productos locales',
+    'description': 'El modulo permite organizar el trabajo de voluntarios de una tienda que vende productos locales.',
+    'author': 'Ismael Simbana',
+    'category': 'Ecommerce',
+    'version': '1.0',
+    'data': [
+    ],
+    'demo': [],
+}
